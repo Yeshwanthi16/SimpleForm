ad00ec754d3eedafb9ecf85b0734ff520dead66d
yeshwanth-dks <yeshwanth.idiga@nextuple.com>
2023-05-24 17:11:46 +0530

    button, Card changes

---
+++ src/Components/UI/Card.js
@@ -0,0 +1,10 @@
+import classses from'./Card.module.css'
+
+const Card = (props) => {
+
+    return (
+        <div className={`${classses.card} ${props.className}`}>{props.children}</div>
+    )
+
+}
+export default Card;
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React , { useState } from 'react'
+import Card from '../UI/Card'
+import classes from './AddUser.module.css'
+import Button from '../UI/Button'
 
 
 const AddUser = (props) => {
 
+   const [enteredUsername,setEnteredUserName] =  useState('');
+   const [enteredAge,setEnteredAge] =  useState('');
+
     const onSubmitHandler = (event) => {
         event.preventDefault()
+        console.log(enteredUsername,enteredAge)
+    }
+
+    const userNameChangehandler = (event) => {
+        setEnteredUserName(event.target.value)
+    }
+
+    const ageChangehandler = (event) => {
+        setEnteredAge(event.target.value)
     }
     
     return(
+<Card className={classes.input}>
     <form onSubmit={onSubmitHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangehandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={ageChangehandler}/>
+        <Button type="submit">Add User</Button>
     </form>
+</Card>    
     )
 
 }
